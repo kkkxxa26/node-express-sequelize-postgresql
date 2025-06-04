4c08935d6b3ef441af74a7e8fc082ee0276a5a32
ICY <kydykbaeva00@gmail.com>
2025-06-05 00:01:52 +0600

    problema test ne reshena

--- app/routes/lab.routes.js
+++ app/routes/lab.routes.js
@@ -3,26 +3,26 @@ module.exports = app => {
   var router = require("express").Router(); 
 
   // Получение данных
-  router.get("/allHead", lab.findAllHead);           // Получить все заголовки заданий
-  router.get("/oneHead/:id", lab.findOneHead);       // Получить один заголовок по ID
-  router.get("/allBody", lab.findAllBody);           // Получить все тела заданий
-  router.get("/allAnswer", lab.findAllAnswer);       // Получить все ответы
+  router.get("/allHead", lab.findAllHead);           
+  router.get("/oneHead/:id", lab.findOneHead);       
+  router.get("/allBody", lab.findAllBody);           
+  router.get("/allAnswer", lab.findAllAnswer);       
   router.get("/user-stats", lab.getUserAnswersStats);
 
   // Создание данных
-  router.post("/head", lab.createHead);              // Создать заголовок задания
-  router.post("/body", lab.createBody);              // Создать тело задания
-  router.post("/answer", lab.createAnswers);         // Добавить варианты ответов
+  router.post("/head", lab.createHead);              
+  router.post("/body", lab.createBody);              
+  router.post("/answer", lab.createAnswers);         
 
   // Обновление данных
-  router.put("/head/:id", lab.updateHead);           // Обновить заголовок задания
-  router.put("/body/:id", lab.updateBody);           // Обновить тело задания
-  router.put("/answer/:id", lab.updateAnswer);       // Обновить ответ
+  router.put("/head/:id", lab.updateHead);           
+  router.put("/body/:id", lab.updateBody);           
+  router.put("/answer/:id", lab.updateAnswer);       
 
   // Удаление данных
-  router.delete("/head/:id", lab.deleteHead);        // Удалить заголовок
-  router.delete("/body/:id", lab.deleteBody);        // Удалить тело задания
-  router.delete("/answer/:id", lab.deleteAnswer);    // Удалить ответ
+  router.delete("/head/:id", lab.deleteHead);       
+  router.delete("/body/:id", lab.deleteBody);        
+  router.delete("/answer/:id", lab.deleteAnswer);   
 
   // Префикс маршрутов
   app.use("/api/lab", router); 
